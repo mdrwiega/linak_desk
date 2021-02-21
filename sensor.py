d9c2e7b24c70c69fc5c4cfd5a5f48f8908601ff4
Michal Drwiega <drwiega.michal@gmail.com>
2021-02-21 21:16:19 +0000

    Integrated sensor platform with the linak driver (read height)

--- sensor.py
+++ sensor.py
@@ -1,15 +1,14 @@
-"""Platform for sensor integration."""
-from homeassistant.const import TEMP_CELSIUS
+""" The Linak desk height sensor platform"""
+
 from homeassistant.helpers.entity import Entity
 
+from .linak_desk import LinakDesk
 
 def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the sensor platform."""
-    add_entities([ExampleSensor()])
+    add_entities([LinakDeskSensor()])
 
-
-
-class ExampleSensor(Entity):
+class LinakDeskSensor(Entity):
     """Representation of a Sensor."""
 
     def __init__(self):
@@ -19,7 +18,7 @@ class ExampleSensor(Entity):
     @property
     def name(self):
         """Return the name of the sensor."""
-        return 'Example Temperature'
+        return 'Linak Desk Height'
 
     @property
     def state(self):
@@ -29,10 +28,12 @@ class ExampleSensor(Entity):
     @property
     def unit_of_measurement(self):
         """Return the unit of measurement."""
-        return TEMP_CELSIUS
+        return 'm'
 
     def update(self):
         """Fetch new state data for the sensor.
         This is the only method that should fetch new data for Home Assistant.
         """
-        pass
+        desk = LinakDesk('DD:6D:C2:F4:39:7F')
+        height, speed = desk.read_desk_height_speed()
+        self._state = height
